9bc6c12ff84fbc8257bf205a5c727af95ee76fce
Vaibhav Kumbhar <vkumbhar94@gmail.com>
2022-09-30 17:16:20 +0530

    fix(csc): add tf comments (#103)

--- scripts/lmtf/main.go
+++ scripts/lmtf/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/logicmonitor/helm-charts-qa/scripts/lmtf/pkg/load"
 	"github.com/logicmonitor/helm-charts-qa/scripts/lmtf/pkg/tmpl"
 	"github.com/logicmonitor/helm-charts-qa/scripts/lmtf/pkg/vardef"
+	"gopkg.in/yaml.v3"
+	"io/ioutil"
 	"os"
 )
 
@@ -12,6 +14,19 @@ func main() {
 	if len(os.Args) < 4 {
 		fmt.Println("Insufficient params: <path> <tmpl file> <var file>")
 		return
+	}
+	bytes, err := ioutil.ReadFile(os.Args[1] + "/Chart.yaml")
+	cm := map[string]any{}
+	if err == nil {
+		err = yaml.Unmarshal(bytes, &cm)
+		if err != nil {
+			fmt.Printf("unable to read Chart.yaml to determine version: %s\n", err)
+			return
+		}
+	}
+	version := ""
+	if v, ok := cm["version"]; ok {
+		version = v.(string)
 	}
 	m, err := load.WalkSchema(os.Args[1])
 	if err != nil {
@@ -25,6 +40,8 @@ func main() {
 	err = os.WriteFile(os.Args[2], []byte(tmplStr), os.ModePerm)
 	varDef := vardef.ProcessVarDef(m, "")
 	defs := vardef.Dump(varDef)
+
+	defs = fmt.Sprintf("%s\nvariable \"lm_container_version\" {\n  type = string\n  default = \"%s\"\n}", defs, version)
 
 	err = os.WriteFile(os.Args[3], []byte(defs), os.ModePerm)
 
